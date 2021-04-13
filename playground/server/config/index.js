f5cdf631464a6b53b839653e18ad2654c2e3783c
Daniel Khan <daniel@khan.io>
2021-04-13 23:44:03 +0200

    Use docker port for mongodb

--- playground/server/config/index.js
+++ playground/server/config/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 module.exports = {
   database: {
-    dsn: 'mongodb://localhost:27017/linkedin-node-authentication',
+    dsn: 'mongodb://localhost:37017/linkedin-node-authentication',
     status: {
       connected: false,
       error: false,
