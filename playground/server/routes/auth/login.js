ae03237b70977bf09c263b1b1394def0a6350078
Daniel Khan <daniel@khan.io>
2021-04-18 21:48:29 +0200

    extend login startingpoint

--- playground/server/routes/auth/login.js
+++ playground/server/routes/auth/login.js
@@ -1,4 +1,6 @@
 const { Router } = require('express');
+// eslint-disable-next-line no-unused-vars
+const UserService = require('../../services/UserService');
 
 const router = Router();
 
@@ -15,6 +17,19 @@ module.exports = () => {
    */
   router.post('/login', async (req, res, next) => {
     try {
+      const errors = [];
+      /**
+       * @todo: Try to find the user in the database and try to validate the password
+       */
+
+      if (errors.length) {
+        // Render the page again and show the errors
+        return res.render('auth/login', {
+          page: 'login',
+          data: req.body,
+          errors,
+        });
+      }
       /**
        * @todo: Log the user in by saving the userid to the session and redirect to the index page
        * @todo: Don't forget about 'Remember me'!
